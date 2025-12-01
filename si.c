89ae2af145976dcb0f8fdac51ce424a552aa8425
RAIK Hamou <raikhamou@gmail.com>
2025-12-01 14:32:52 +0100

    ajout des fonctions dans si.c

--- si.c
+++ si.c
@@ -1,1 +1,53 @@
+#include <stdio.h>
 
+char matrice[5][11]=
+{
+  {1,1,1,1,1,1,1,1,1,1,1},
+  {2,2,2,2,2,2,2,2,2,2,2},
+  {2,2,2,2,2,2,2,2,2,2,2},
+  {3,3,3,3,3,3,3,3,3,3,3},
+  {3,3,3,3,3,3,3,3,3,3,3}
+};
+
+/* renvoie la matrice */
+ char *si_get_matrix(void)
+ {
+   char *matrix=&matrice[0][0];
+   return matrix;
+ }
+   
+ /* renvoie le nombre d’ennemis restant dans la matrice */
+ int si_matrix_count(void)
+ {
+   int ennemis=0;
+   for(int i=0;i<5;i++)
+     {
+       for(int j=0;j<11;j++)
+	 {
+	   if (matrice[i][j]!=0)
+	     {
+	       ennemis+=1;
+	     } 
+	 }
+     }
+   return ennemis;
+ }
+
+/* renvoi le nombre de points en fonction de l’ennemi 
+int si_get_points(Si_Type t)
+{
+  
+}*/
+
+/* renvoie le nombre de joueurs 
+int si_get_nbr_players(Si *si)
+{
+
+}*/
+
+int main()
+{
+  int a=si_matrix_count();
+  printf("%d",a);
+  return 0;
+}
--- si.h
+++ si.h
@@ -45,3 +45,66 @@ struct Si
   Invaders invaders;
 };
 
+ /*
+ * crée un pointeur vers la structure Si. On passe en paramètre
+ * les dimension de la fenêtre et la taille de pixel
+ */
+ Si *si_new(int window_width, int window_height, int pixel_size);
+
+/*Détruit les ressources */
+void si_sdl(Si *si);
+
+/*renvoie la matrice */
+char *si_get_matrix(void);
+
+/*renvoi le nombre d'ennemis restant dans la matrice */
+int si_matrix_count(void);
+
+/*renvoi le nombre de point en fonction de l'ennemi*/
+int si_get_points(Si_Type t);
+
+/*tank*/
+
+/*
+ * teste si le tank peut se d\’eplacer vers le haut. SI c’est possible,
+ * met à jour la coordonné y du tir da Tank.
+ */
+ int si_tank_shoot_can_move_up(Si *si);
+
+ /* renvoie 1 si le tank est touch\’e, 0 sinon */
+ int si_tank_is_hit(Si *si);
+
+
+ /* invaders */
+
+ /*
+ * Détermine dans quelle colonnnede la matrice, choisie aléatoirement,
+ * la bombe est lachée. Met à jour les coordonn\’ees de la bombe
+ * dans Invaders.
+ */
+ void si_invaders_get_column(Si *si);
+
+ /*
+ * Renvoie 1 si la bombe peut se déplacer vers le bas, 0 sinon.
+ * Met à jour la coordonnéee y de la bombe.
+ */
+ int si_invaders_bomb_can_move_down(Si *si);
+
+ /*
+ * Renvoie 1 si les ennemis peuvent se déplacer vers la gauche, 0 sinon.
+ * Met à jour la position de la matrice.
+ */
+
+ int si_invaders_can_move_left(Si *si);
+ /*
+ * Renvoie 1 si les ennemis peuvent se déplacer vers la droite, 0 sinon.
+ * Met à jour la position de la matrice.
+ */
+
+ int si_invaders_can_move_right(Si *si);
+
+ /*
+ * Renvoie 1 si un ennemi est touché par le tir du tank, 0 sinon.
+ * Met à jour la matrice.
+ */
+ int si_invader_is_hit(Si *si);
