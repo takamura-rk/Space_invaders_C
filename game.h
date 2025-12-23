08cf01f99d197fe975e039752506e10ed763694e
RAIK Hamou <raikhamou@gmail.com>
2025-12-23 18:46:33 +0100

    modif

--- game.h
+++ game.h
@@ -32,3 +32,36 @@ void   game_run(Game *g);
 void   game_del(Game *g);
 
 #endif
+#ifndef GAME_H
+#define GAME_H
+
+#include <SDL.h>
+#include "si.h"
+
+
+typedef struct
+{
+  SDL_Window *win;
+  SDL_Renderer *ren;
+  
+  int window_width;
+  int window_height;
+  int pixel_size;
+  int play_game;
+  
+  Si *si;
+  
+  Uint64 freq;
+  Uint64 count_invaders;
+  Uint64 count_shoot;
+  
+  char update;
+} Game;
+
+Game *game_new();
+void game_update(Game *g);
+
+void game_run(Game *g);
+void game_del(Game *g);
+
+#endif
